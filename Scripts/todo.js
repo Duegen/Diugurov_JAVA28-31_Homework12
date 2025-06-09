60ba7dda7aa3c7e668666a973c7920c081c7c4a5
Duegen <nikitadyugurov@gmail.com>
2025-06-09 22:20:26 +0300

    Final version 1.0.3

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -13,15 +13,18 @@ clear_btn.onclick = () => {
 clear_all_btn.onclick = () => {
     let result = window.confirm("Do you want to delete ALL tasks?");
 
-    if (result) {
-        while(tasks.childNodes.length)
-            tasks.childNodes[0].remove()
-    }
+    if (result) tasks.innerHTML=""
     not_found.style.display = "block";
     document.querySelector("#radio_all").checked = true;
 }
 
-function add_task() {
+function createBtn(handle) {
+    const new_btn = document.createElement("btn")
+    new_btn.onclick = handle
+    return new_btn
+}
+
+function handle_add_task() {
     if (!(task_text.value)) return;
 
     const new_task = document.createElement("li");
@@ -40,38 +43,40 @@ function add_task() {
     new_task.append(new_task_text);
     new_task_text.innerHTML = task_text.value;
 
-    const new_task_close = document.createElement("img")
-    new_task_close.className = "img_close"
-    new_task_close.src = "./Styles/Images/icon_close.png"
-    new_task.append(new_task_close);
-    task_text.value = "";
-
-    new_task_close.addEventListener("click", function () {
-        this.parentElement.remove()
+    function handle_close_task() {
+        new_task.remove()
 
         const tasks_done = document.querySelectorAll(".done")
         const tasks_undone = document.querySelectorAll(".undone")
         const checked = document.querySelector('input[name="select"]:checked');
 
-        (checked.value === "radio_all" && (!tasks_done.length || !tasks_undone.length)) || (!tasks_undone.length && checked.value === "radio_active") ||
+        (checked.value === "radio_all" && (!(tasks_done.length + tasks_undone.length))) ||
+        (!tasks_undone.length && checked.value === "radio_active") ||
         (!tasks_done.length && checked.value === "radio_done")
             ? not_found.style.display = "block" : not_found.style.display = "none";
-    })
+    }
+
+    const new_task_close = createBtn(handle_close_task)
+    new_task_close.className = "img_close"
+    new_task.append(new_task_close);
+
+    task_text.value = "";
+
     new_task_close.addEventListener("mouseover", function () {
-        this.parentElement.classList.add("border")
+        new_task.classList.add("border")
     })
     new_task_close.addEventListener("mouseout", function () {
-        this.parentElement.classList.remove("border")
+        new_task.classList.remove("border")
     })
     new_task_check.addEventListener("change", function () {
-        this.parentElement.children[1].classList.toggle("text_through");
-        this.parentElement.classList.toggle("done");
-        this.parentElement.classList.toggle("undone");
+        new_task.children[1].classList.toggle("text_through");
+        new_task.classList.toggle("done");
+        new_task.classList.toggle("undone");
 
         const checked = document.querySelector('input[name="select"]:checked');
-        (this.parentElement.classList.contains("done") && checked.value === "radio_active") ||
-        (this.parentElement.classList.contains("undone") && checked.value === "radio_done") ?
-            this.parentElement.style.display = "none" : this.parentElement.style.display = "flex"
+        (new_task.classList.contains("done") && checked.value === "radio_active") ||
+        (new_task.classList.contains("undone") && checked.value === "radio_done") ?
+            new_task.style.display = "none" : new_task.style.display = "flex"
 
         const tasks_done = document.querySelectorAll(".done");
         const tasks_undone = document.querySelectorAll(".undone");
@@ -81,7 +86,7 @@ function add_task() {
     })
 }
 
-add_btn.onclick = add_task;
+add_btn.onclick = handle_add_task;
 
 for (let i = 0; i < radio_btns.length; i++) {
     radio_btns[i].onchange = function () {
